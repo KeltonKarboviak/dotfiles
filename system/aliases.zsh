53cfca987bdbb391febdf63ab91028f43b0266e9
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-12-19 16:32:18 -0600

    Modified existing files for my personal use

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,12 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# Add an "alert" alias for long running commands. Use like so:
+#    sleep 10; alert;
+alias alert='notify-send --urgency=low -i "$([ $? = 0] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alias pbcopy='xclip -sel clip'
+alias sshconfig='e ~/.ssh/config'
+alias zshconfig='e $ZSH/.zshrc'
+alias ohmyzsh='e ~/.oh-my-zsh'
--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,8 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+# Add each topic folder to fpath so that they can add functions and completion scripts
+for topic_folder ($ZSH/*)
+do
+  if [ -d $topic_folder ]
+  then
+    fpath=($topic_folder $fpath);
+  fi
+done
